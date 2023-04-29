4053e92f2e27e0a3bf0ce45e5f130ea22873253c
Austin <austin@AustindeMacBook-Pro.local>
2023-04-29 17:35:29 -0400

    fb communicate

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,8 +6,10 @@ const app = express();
 // Enable CORS
 app.use(cors());
 
+// this is for test 
 app.get('/api/test', (req, res) => {
   res.json({ message: 'Hello from the backend!' });
 });
+// above is for test 
 
 module.exports = app;
