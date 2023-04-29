c3a0e2255559629af7e309aec72f8351f71c90ef
Austin <austin@AustindeMacBook-Pro.local>
2023-04-29 17:35:00 -0400

    communication

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -3,6 +3,20 @@ import { useLocation } from 'react-router-dom';
 import NavBar from '../components/NavigationBar';
 
 function HomePage() {
+
+    // this is test information
+    const [message, setMessage] = useState('');
+
+    useEffect(() => {
+      fetch('http://localhost:8080/api/test')
+        .then((response) => response.json())
+        .then((data) => setMessage(data.message))
+        .catch((error) => console.error('Error fetching data:', error));
+    }, []);
+
+    console.log(message)
+    // above is test information
+
     const location = useLocation();
     // check if the user is logged in
     const [user, setUser] = useState(false);
