5bf9cdfc2cc7ee8b9d0202df4a60306c45330ac6
yaoluxun <yaoluxun2006@gmail.com>
2023-05-05 18:15:03 -0400

    remove maqrquenn

--- frontend/src/pages/Review.js
+++ frontend/src/pages/Review.js
@@ -3,10 +3,8 @@ import { useLocation } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import NavBar from '../components/NavigationBar';
 import Button from '@material-ui/core/Button';
-import ReviewsMarquee from '../components/ReviewsMarquee';
 import TextField from '@material-ui/core/TextField';
 import { getCookie } from '../components/GetCookie';
-import Marquee from 'react-marquee';
 
 function ReviewPage() {
   const [user, setUser] = useState(false);
